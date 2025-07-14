58b813bd3b84a167e021c237ee6f6f2fd80f8938
Soheil <greenroomblackroom@gmail.com>
2025-07-14 23:54:05 +0330

    Configure admin interface for all models again

--- customer/admin.py
+++ customer/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from customer.models import Customer
+from .models import Customer, Address
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'phone', 'is_seller', 'is_staff']
     list_filter = ['is_seller', 'is_staff', 'is_superuser']
     search_fields = ['username', 'email', 'phone']
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['label', 'user', 'city', 'state', 'postal_code', 'country']
+    search_fields = ['label', 'city', 'state', 'postal_code', 'country']
+    autocomplete_fields = ['user']
--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Cart, CartItem, Order, OrderItem, Payment
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at']
+    search_fields = ['user__username']
+    autocomplete_fields = ['user']
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'store_item', 'quantity', 'unit_price', 'total_item_price', 'total_discount']
+    list_filter = ['cart']
+    search_fields = ['store_item__product__name']
+    autocomplete_fields = ['cart', 'store_item']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'status', 'total_price', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['user__username']
+    autocomplete_fields = ['user', 'address']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'store_item', 'quantity', 'price', 'total_price']
+    search_fields = ['store_item__product__name']
+    autocomplete_fields = ['order', 'store_item']
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['order', 'transaction_id', 'amount', 'fee', 'status', 'created_at']
+    search_fields = ['transaction_id', 'reference_id']
+    autocomplete_fields = ['order']
