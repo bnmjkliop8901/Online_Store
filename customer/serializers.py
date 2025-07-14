9dceb9d90efb6f084d148c8764892d7761b5e253
Soheil <greenroomblackroom@gmail.com>
2025-07-15 00:11:20 +0330

    make somw changes

--- customer/serializers.py
+++ customer/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 from .models import Customer, Address
 
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = ['id', 'username', 'email', 'phone', 'picture', 'is_seller']
-
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = '__all__'
+        read_only_fields = ('user',)
+
+class AddressCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        exclude = ['user']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+
+class CustomerSerializer(serializers.ModelSerializer):
+    addresses = AddressSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Customer
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name', 'phone', 'addresses'
+        ]
+        read_only_fields = ['id']
